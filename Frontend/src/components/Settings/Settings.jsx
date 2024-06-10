98bcbf8866aa76e5efc760914319ef3a581ae0b6
Epereira2021576 <134961772+Epereira2021576@users.noreply.github.com>
2024-06-10 10:53:07 -0600

    Refactor import statement in Settings component

--- Frontend/src/components/Settings/Settings.jsx
+++ Frontend/src/components/Settings/Settings.jsx
@@ -1,5 +1,6 @@
 import { useChannelSettings } from "../../shared/hooks";
-import { ChannelSettings } from "../channel/ChannelSettings.jsx";
+import ChannelSettings from '../channel/ChannelSettings.jsx';
+
 import { LoadingSpinner } from '../LoadingSpinner'
 import { PasswordSettings } from "./PasswordSettings";
 import { StreamKey } from "./StreamKey";
