f6de63c322be68d380f010f68774fbf8fd0a1448
Epereira2021576 <134961772+Epereira2021576@users.noreply.github.com>
2024-06-10 10:50:22 -0600

    im tired

--- Frontend/src/components/Settings/Settings.jsx
+++ Frontend/src/components/Settings/Settings.jsx
@@ -1,5 +1,5 @@
 import { useChannelSettings } from "../../shared/hooks";
-import { ChannelSettings } from "../channel/ChannelSettings";
+import { ChannelSettings } from "../channel/ChannelSettings.jsx";
 import { LoadingSpinner } from '../LoadingSpinner'
 import { PasswordSettings } from "./PasswordSettings";
 import { StreamKey } from "./StreamKey";
